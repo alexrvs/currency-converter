838eed39c9935753d5e66d7d2d625dc229751d96
alexrvs <vasyutin4@gmail.com>
2020-06-11 19:04:12 +0300

    rspec tests

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,10 +1,17 @@
 # spec/app_spec.rb
 require File.expand_path '../spec_helper.rb', __FILE__
 
-describe "My Sinatra Application" do
+describe "Currency Converter Application" do
   it "should allow accessing the home page" do
-    get '/'
-    # Rspec 2.x
-    expect(last_response).to be_ok
+    get "/"
+
+    expect(last_response.body).to eq("")
+    expect(last_response.status).to eq 200
+
   end
+
+  it "should re"
 end
+
+
+
